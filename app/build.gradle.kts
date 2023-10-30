58f399d830956f36622abfe3c36685245c9d6ac8
params19 <paramsrivastava5@gmail.com>
2023-10-29 23:41:07 -0700

    Updated with Final Front Page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.medicare_app_param"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.medicare_app_param"
