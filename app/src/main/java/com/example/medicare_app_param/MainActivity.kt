6a30c77ad2fefe4cf9a3a0cd3307701bfe0a77f5
params19 <paramsrivastava5@gmail.com>
2023-10-30 18:08:03 -0700

    Updated with second page and transitions

--- app/src/main/java/com/example/medicare_app_param/MainActivity.kt
+++ app/src/main/java/com/example/medicare_app_param/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.medicare_app_param
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val signIn=findViewById<TextView>(R.id.textView8)
+        signIn.setOnClickListener{
+            val intent= Intent(this,Sign_in_page::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/medicare_app_param/Sign_in_page.kt
@@ -0,0 +1,11 @@
+package com.example.medicare_app_param
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign_in_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_page)
+    }
+}
