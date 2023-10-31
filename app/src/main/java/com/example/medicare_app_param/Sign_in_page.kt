623868e6bd800557bc597a7e4187af06e0901d31
params19 <paramsrivastava5@gmail.com>
2023-10-31 16:33:18 -0700

    Updated with third page and elements added

--- app/src/main/java/com/example/medicare_app_param/Sign_in_page.kt
+++ app/src/main/java/com/example/medicare_app_param/Sign_in_page.kt
@@ -1,11 +1,19 @@
 package com.example.medicare_app_param
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Sign_in_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in_page)
+
+        val signIn = findViewById<TextView>(R.id.button)
+        signIn.setOnClickListener {
+            val intent = Intent(this, intro_page::class.java)
+            startActivity(intent)
+        }
     }
 }
