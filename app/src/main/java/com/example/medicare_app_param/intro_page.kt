ff194f956dff2718260b5e2d7213a3a5ef5d69f0
params19 <paramsrivastava5@gmail.com>
2023-11-01 11:33:45 -0700

    Fourth slide modified with elements and details(bottom nav bar)

--- app/src/main/java/com/example/medicare_app_param/intro_page.kt
+++ app/src/main/java/com/example/medicare_app_param/intro_page.kt
@@ -1,11 +1,20 @@
 package com.example.medicare_app_param
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class intro_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro_page)
+
+        val aboutDoc=findViewById<TextView>(R.id.doctor_)
+        aboutDoc.setOnClickListener{
+            val intent= Intent(this,About_doctors::class.java)
+            startActivity(intent)
+        }
+
     }
 }
