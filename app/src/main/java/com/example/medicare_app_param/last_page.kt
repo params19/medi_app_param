6ac270232fd55e2039b0b23a126775fb2817946d
params19 <paramsrivastava5@gmail.com>
2023-11-01 13:04:32 -0700

    Last slide upadted with text and images

---
+++ app/src/main/java/com/example/medicare_app_param/last_page.kt
@@ -0,0 +1,11 @@
+package com.example.medicare_app_param
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class last_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_last_page)
+    }
+}
